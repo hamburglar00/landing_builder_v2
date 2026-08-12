499d1ecba9b97e120550015336209f225a876231
hamburglar00 <hamburglar002@proton.me>
2026-08-12 10:17:51 -0300

    Refine WhatsApp inbox doodle background

--- frontend/components/whatsapp-cloud-api/WhatsAppCloudApiInboxPageContent.tsx
+++ frontend/components/whatsapp-cloud-api/WhatsAppCloudApiInboxPageContent.tsx
@@ -33,49 +33,85 @@ const TAG_CLASSES: Record<WhatsappCloudApiInboxThread["tag"], string> = {
 };
 
 const WHATSAPP_DOODLE_PATTERN = encodeURIComponent(`
-<svg xmlns="http://www.w3.org/2000/svg" width="220" height="220" viewBox="0 0 220 220">
-  <g fill="none" stroke="#8696a0" stroke-width="1.25" stroke-linecap="round" stroke-linejoin="round" opacity=".14">
-    <path d="M15 16h22v15H24l-9 7V16Z"/><path d="M22 22h9M22 27h6"/>
-    <path d="M58 13c8 0 14 6 14 13s-6 13-14 13-14-6-14-13 6-13 14-13Z"/><path d="M53 24h.1M63 24h.1M53 30c4 3 9 3 13 0"/>
-    <path d="M96 15h26v18H96z"/><path d="m96 15 13 10 13-10"/>
-    <path d="M154 14c8 0 14 5 14 12s-6 12-14 12h-9l-7 7v-11c-4-2-7-5-7-9 0-6 6-11 14-11Z"/><path d="M147 23h12M147 29h8"/>
-    <path d="M192 13 207 28l-15 15-15-15 15-15Z"/><path d="M187 28h10M192 23v10"/>
-    <path d="M18 62c8-9 23-4 22 7-1 9-12 13-21 7l-9 3 3-8c-2-4 0-7 5-9Z"/>
-    <path d="M60 58h20v24H60z"/><path d="M65 64h10M65 70h7"/>
-    <path d="M108 59c8 0 15 6 15 14s-7 14-15 14-15-6-15-14 7-14 15-14Z"/><path d="M102 73h12M108 67v12"/>
-    <path d="M153 59h21v21h-21z"/><path d="M158 64h11M158 70h8M158 76h5"/>
-    <path d="M196 58c7 0 13 6 13 13s-6 13-13 13h-15V58h15Z"/><path d="M188 67h10M188 74h6"/>
-    <path d="M20 106h17l8 14H27l-7-14Z"/><path d="M20 106l7-10h17l-7 10"/>
-    <path d="M68 99c7 0 12 5 12 12v6H56v-6c0-7 5-12 12-12Z"/><path d="M60 117v8h16v-8"/>
-    <path d="M109 101 125 117l-16 16-16-16 16-16Z"/><path d="M103 117h12M109 111v12"/>
-    <path d="M154 100c6-9 21-5 21 6 0 9-11 15-20 9-7 7-18 2-18-8 0-9 12-14 17-7Z"/>
-    <path d="M197 99c8 0 14 6 14 13s-6 13-14 13-14-6-14-13 6-13 14-13Z"/><path d="M191 108h12M191 115h8"/>
-    <path d="M18 151h24v17H18z"/><path d="M22 156h16M22 162h10"/>
-    <path d="M66 145c9 0 16 6 16 14 0 9-7 15-16 15l-8 8v-10c-5-2-8-7-8-13 0-8 7-14 16-14Z"/>
-    <path d="M109 146c8 0 14 6 14 14s-6 14-14 14-14-6-14-14 6-14 14-14Z"/><path d="M104 157h.1M114 157h.1M104 164c4 3 8 3 12 0"/>
-    <path d="M152 146h24v18h-24z"/><path d="m152 146 12 11 12-11"/>
-    <path d="M197 146c7 0 13 5 13 12s-6 12-13 12h-15v-24h15Z"/><path d="M188 154h11M188 160h7"/>
-    <path d="M27 193c0-7 6-13 13-13s13 6 13 13-6 13-13 13H27v-13Z"/><path d="M34 190h10M34 196h7"/>
-    <path d="M82 183 94 195l-12 12-12-12 12-12Z"/>
-    <path d="M119 182h24v18h-24z"/><path d="M124 188h14M124 194h9"/>
-    <path d="M168 183c8 0 14 5 14 12s-6 12-14 12-14-5-14-12 6-12 14-12Z"/>
-    <path d="M201 181h9v26h-9z"/><path d="M197 186h17M197 202h17"/>
+<svg xmlns="http://www.w3.org/2000/svg" width="170" height="170" viewBox="0 0 170 170">
+  <defs>
+    <symbol id="chat" viewBox="0 0 24 24"><path d="M4 5.5A2.5 2.5 0 0 1 6.5 3h11A2.5 2.5 0 0 1 20 5.5v8A2.5 2.5 0 0 1 17.5 16H9l-5 4V5.5Z"/><path d="M8 8h8M8 11.5h5"/></symbol>
+    <symbol id="face" viewBox="0 0 24 24"><circle cx="12" cy="12" r="8"/><path d="M8.5 10h.1M15.5 10h.1M8.5 15c2.5 2 4.5 2 7 0"/></symbol>
+    <symbol id="heart" viewBox="0 0 24 24"><path d="M12 20s-8-4.8-8-10.5C4 6.6 6.2 5 8.4 5c1.5 0 2.8.8 3.6 2 .8-1.2 2.1-2 3.6-2C17.8 5 20 6.6 20 9.5 20 15.2 12 20 12 20Z"/></symbol>
+    <symbol id="star" viewBox="0 0 24 24"><path d="m12 3 2.5 5.6 6.1.6-4.6 4.1 1.3 6-5.3-3.1-5.3 3.1 1.3-6-4.6-4.1 6.1-.6L12 3Z"/></symbol>
+    <symbol id="phone" viewBox="0 0 24 24"><path d="M8 4h8a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H8a2 2 0 0 1-2-2V6a2 2 0 0 1 2-2Z"/><path d="M10 17h4"/></symbol>
+    <symbol id="camera" viewBox="0 0 24 24"><path d="M5 8h3l1.5-2h5L16 8h3a2 2 0 0 1 2 2v7a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-7a2 2 0 0 1 2-2Z"/><circle cx="12" cy="13" r="3"/></symbol>
+    <symbol id="gift" viewBox="0 0 24 24"><path d="M4 10h16v10H4V10ZM3 7h18v3H3V7ZM12 7v13"/><path d="M12 7c-4 0-4-4-1.5-4 1.5 0 1.5 2 1.5 4Zm0 0c4 0 4-4 1.5-4C12 3 12 5 12 7Z"/></symbol>
+    <symbol id="mail" viewBox="0 0 24 24"><path d="M4 6h16v12H4z"/><path d="m4 6 8 7 8-7"/></symbol>
+    <symbol id="music" viewBox="0 0 24 24"><path d="M9 18V6l10-2v12"/><circle cx="6.5" cy="18" r="2.5"/><circle cx="16.5" cy="16" r="2.5"/></symbol>
+    <symbol id="clock" viewBox="0 0 24 24"><circle cx="12" cy="12" r="8"/><path d="M12 7v5l3 2"/></symbol>
+    <symbol id="spark" viewBox="0 0 24 24"><path d="M12 3v6M12 15v6M3 12h6M15 12h6M6 6l4 4M14 14l4 4M18 6l-4 4M10 14l-4 4"/></symbol>
+    <symbol id="pin" viewBox="0 0 24 24"><path d="M12 21s6-6 6-11a6 6 0 0 0-12 0c0 5 6 11 6 11Z"/><circle cx="12" cy="10" r="2"/></symbol>
+    <symbol id="bolt" viewBox="0 0 24 24"><path d="M13 2 5 13h6l-1 9 9-13h-6l1-7Z"/></symbol>
+    <symbol id="cup" viewBox="0 0 24 24"><path d="M6 8h10v5a5 5 0 0 1-10 0V8Z"/><path d="M16 9h2a2.5 2.5 0 0 1 0 5h-2M7 20h8M9 4c-1 1-1 2 0 3M13 4c-1 1-1 2 0 3"/></symbol>
+    <symbol id="check" viewBox="0 0 24 24"><path d="M4 12.5 9 17 20 6"/></symbol>
+    <symbol id="bag" viewBox="0 0 24 24"><path d="M6 8h12l-1 12H7L6 8Z"/><path d="M9 8a3 3 0 0 1 6 0"/></symbol>
+    <symbol id="play" viewBox="0 0 24 24"><circle cx="12" cy="12" r="8"/><path d="m10 8 6 4-6 4V8Z"/></symbol>
+  </defs>
+  <g fill="none" stroke="#8696a0" stroke-width="1.15" stroke-linecap="round" stroke-linejoin="round" opacity=".145">
+    <use href="#chat" x="4" y="7" width="17" height="17" transform="rotate(-11 12 15)"/>
+    <use href="#heart" x="35" y="4" width="12" height="12" transform="rotate(15 41 10)"/>
+    <use href="#camera" x="66" y="9" width="15" height="15" transform="rotate(-7 73 16)"/>
+    <use href="#spark" x="111" y="2" width="12" height="12" transform="rotate(28 117 8)"/>
+    <use href="#face" x="139" y="8" width="18" height="18" transform="rotate(9 148 17)"/>
+    <use href="#music" x="22" y="31" width="14" height="14" transform="rotate(16 29 38)"/>
+    <use href="#gift" x="52" y="27" width="16" height="16" transform="rotate(-12 60 35)"/>
+    <use href="#pin" x="91" y="33" width="12" height="12" transform="rotate(6 97 39)"/>
+    <use href="#mail" x="123" y="28" width="17" height="17" transform="rotate(-16 131 36)"/>
+    <use href="#bolt" x="158" y="36" width="10" height="10" transform="rotate(22 163 41)"/>
+    <use href="#clock" x="7" y="57" width="13" height="13" transform="rotate(8 13 63)"/>
+    <use href="#star" x="38" y="58" width="13" height="13" transform="rotate(-18 44 64)"/>
+    <use href="#phone" x="73" y="53" width="16" height="16" transform="rotate(12 81 61)"/>
+    <use href="#cup" x="104" y="61" width="13" height="13" transform="rotate(-9 110 67)"/>
+    <use href="#chat" x="138" y="55" width="15" height="15" transform="rotate(17 145 62)"/>
+    <use href="#bag" x="16" y="84" width="14" height="14" transform="rotate(-14 23 91)"/>
+    <use href="#spark" x="51" y="80" width="11" height="11" transform="rotate(4 56 85)"/>
+    <use href="#play" x="83" y="87" width="13" height="13" transform="rotate(-21 89 93)"/>
+    <use href="#heart" x="116" y="82" width="12" height="12" transform="rotate(10 122 88)"/>
+    <use href="#camera" x="147" y="90" width="15" height="15" transform="rotate(-10 154 97)"/>
+    <use href="#mail" x="3" y="119" width="15" height="15" transform="rotate(12 10 126)"/>
+    <use href="#face" x="34" y="108" width="15" height="15" transform="rotate(-8 41 115)"/>
+    <use href="#check" x="66" y="122" width="13" height="13" transform="rotate(19 72 128)"/>
+    <use href="#music" x="95" y="111" width="13" height="13" transform="rotate(-18 101 117)"/>
+    <use href="#gift" x="128" y="121" width="14" height="14" transform="rotate(8 135 128)"/>
+    <use href="#pin" x="154" y="115" width="12" height="12" transform="rotate(-13 160 121)"/>
+    <use href="#bolt" x="12" y="150" width="10" height="10" transform="rotate(-21 17 155)"/>
+    <use href="#cup" x="45" y="144" width="14" height="14" transform="rotate(14 52 151)"/>
+    <use href="#star" x="79" y="151" width="11" height="11" transform="rotate(26 84 156)"/>
+    <use href="#phone" x="109" y="139" width="14" height="14" transform="rotate(-8 116 146)"/>
+    <use href="#chat" x="139" y="145" width="16" height="16" transform="rotate(10 147 153)"/>
   </g>
-  <g fill="none" stroke="#8696a0" stroke-width=".95" stroke-linecap="round" stroke-linejoin="round" opacity=".1">
-    <path d="M8 47c5 4 9 4 14 0M46 48l8 8M54 48l-8 8M84 48c4 5 8 6 13 4M130 49h14M176 48l10-7 6 10M209 46c-4 5-4 9 0 14"/>
-    <path d="M9 90h13M43 90c4 5 9 5 13 0M86 90l8-8 8 8M129 90c2 6 7 8 12 6M178 90h13M211 91c-5 3-7 8-4 13"/>
-    <path d="M7 137c6 2 11 0 14-5M45 137l9-9 9 9M86 137h16M129 137c4 4 9 4 13 0M177 137l8 8M185 137l-8 8M210 137c-4 5-4 9 0 14"/>
-    <path d="M9 177c5 4 9 4 14 0M48 178h14M99 179c3 5 7 6 12 4M146 178l8-8 8 8M188 178c6 2 10 0 13-5"/>
-    <path d="M31 6c3 3 6 3 9 0M77 6h12M128 7l6 6M134 7l-6 6M181 7c3 3 6 3 9 0"/>
-    <path d="M35 213h13M71 211c4 4 8 4 12 0M149 212h12M187 212l7-7 7 7"/>
+  <g fill="none" stroke="#8696a0" stroke-width=".85" stroke-linecap="round" stroke-linejoin="round" opacity=".095">
+    <use href="#spark" x="18" y="2" width="8" height="8" transform="rotate(15 22 6)"/>
+    <use href="#check" x="58" y="3" width="9" height="9" transform="rotate(-20 62 7)"/>
+    <use href="#bolt" x="98" y="18" width="8" height="8" transform="rotate(34 102 22)"/>
+    <use href="#heart" x="5" y="39" width="8" height="8" transform="rotate(-24 9 43)"/>
+    <use href="#star" x="82" y="30" width="8" height="8" transform="rotate(11 86 34)"/>
+    <use href="#check" x="144" y="35" width="9" height="9" transform="rotate(-11 148 39)"/>
+    <use href="#spark" x="29" y="70" width="8" height="8" transform="rotate(28 33 74)"/>
+    <use href="#bolt" x="64" y="76" width="8" height="8" transform="rotate(-19 68 80)"/>
+    <use href="#star" x="128" y="71" width="9" height="9" transform="rotate(19 132 75)"/>
+    <use href="#heart" x="155" y="75" width="8" height="8" transform="rotate(-8 159 79)"/>
+    <use href="#check" x="28" y="103" width="9" height="9" transform="rotate(14 32 107)"/>
+    <use href="#spark" x="74" y="105" width="8" height="8" transform="rotate(-31 78 109)"/>
+    <use href="#bolt" x="137" y="105" width="8" height="8" transform="rotate(18 141 109)"/>
+    <use href="#star" x="17" y="135" width="8" height="8" transform="rotate(-13 21 139)"/>
+    <use href="#heart" x="91" y="136" width="8" height="8" transform="rotate(24 95 140)"/>
+    <use href="#spark" x="158" y="145" width="8" height="8" transform="rotate(-22 162 149)"/>
+    <path d="M33 20c5 3 8 3 12 0M118 21c3 5 7 6 11 3M24 52l7 7M31 52l-7 7M114 48h11M43 95c4 4 8 4 12 0M100 101l8-6 7 7M5 160c5 2 8 0 10-4M67 163h10M127 162c4 3 8 3 11 0"/>
   </g>
 </svg>
 `);
 
 const WHATSAPP_CHAT_BACKGROUND: CSSProperties = {
   backgroundColor: "#0b141a",
-  backgroundImage: `linear-gradient(rgba(11, 20, 26, 0.7), rgba(11, 20, 26, 0.7)), url("data:image/svg+xml,${WHATSAPP_DOODLE_PATTERN}")`,
-  backgroundSize: "auto, 220px 220px",
+  backgroundImage: `linear-gradient(rgba(11, 20, 26, 0.66), rgba(11, 20, 26, 0.66)), url("data:image/svg+xml,${WHATSAPP_DOODLE_PATTERN}")`,
+  backgroundSize: "auto, 170px 170px",
   backgroundPosition: "0 0, 0 0",
 };
 
